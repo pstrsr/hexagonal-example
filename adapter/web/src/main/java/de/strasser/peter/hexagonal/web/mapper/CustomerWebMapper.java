1cb5bc49ba954c86ad003a9f21204dfa66c70d0c
Peter Straßer <peter.str@hotmail.de>
2021-04-12 20:57:04 +0200

    added address mapping

--- adapter/web/src/main/java/de/strasser/peter/hexagonal/web/mapper/CustomerWebMapper.java
+++ adapter/web/src/main/java/de/strasser/peter/hexagonal/web/mapper/CustomerWebMapper.java
@@ -1,14 +1,18 @@
 package de.strasser.peter.hexagonal.web.mapper;
 
+import de.strasser.peter.hexagonal.application.customer.domain.Address;
 import de.strasser.peter.hexagonal.application.customer.domain.Customer;
+import de.strasser.peter.hexagonal.web.dto.response.AddressResponse;
 import de.strasser.peter.hexagonal.web.dto.response.CustomerResponse;
 import org.mapstruct.Mapper;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface CustomerWebMapper {
   CustomerResponse toResponse(Customer customer);
 
   List<CustomerResponse> toResponse(List<Customer> customer);
+
 }
